b4a334cb9e7231b120e5c79cc0d36bea328e0645
Joseph Larralde <joseph.larralde@gmail.com>
2023-06-29 01:41:40 +0200

    new commented MacOS pd binary start command

--- src/restart-pd.sh
+++ src/restart-pd.sh
@@ -12,3 +12,4 @@ pd -nogui -audiobuf 20 -blocksize 64 -open $1 &
 # /Applications/Pd-0.48-1-i386.app/Contents/Resources/bin/pd -nogui -audiodev "1 1" -audiobuf 12 -open $1 &
 # /Applications/Pd-0.48-1-i386.app/Contents/Resources/bin/pd -nogui -open $1 &
 # /Applications/Pd-0.50-0.app/Contents/Resources/bin/pd -listdev -nogui -audiodev "1 1" -channels "1 1" -open $1 &
+# /Applications/Pd-0.52-1-really.app/Contents/Resources/bin/pd -listdev -nogui -audiodev "1 1" -channels "1 1" -open $1 &
